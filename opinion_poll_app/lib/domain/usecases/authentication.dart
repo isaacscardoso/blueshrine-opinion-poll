e27289ffed5c9f519e69e28b625a447d360fe771
isaacscardoso <isaacscardosoblue@outlook.com>
2022-11-29 00:21:51 -0300

    Refactoring the abstract class 'Authentication': Adding the class 'AuthenticationParameters' so that each class has its own responsibility. (app)

--- opinion_poll_app/lib/domain/usecases/authentication.dart
+++ opinion_poll_app/lib/domain/usecases/authentication.dart
@@ -1,8 +1,17 @@
 import '../entities/entities.dart';
 
+class AuthenticationParameters {
+  final String email;
+  final String password;
+
+  AuthenticationParameters({
+    required this.email,
+    required this.password,
+  });
+}
+
 abstract class Authentication {
   Future<AccountEntity> authenticate({
-    required String email,
-    required String password,
+    required AuthenticationParameters parameters,
   });
 }
