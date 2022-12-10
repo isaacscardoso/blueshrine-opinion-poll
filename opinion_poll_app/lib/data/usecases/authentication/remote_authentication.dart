7e9fd681ed7ea3932a8d808a8e4adf1c07321bfc
isaacscardoso <isaacscardosoblue@outlook.com>
2022-12-09 23:11:34 -0300

    Implementation of the layer 'Authenticaion' contained in the layer 'Usecases' within the layer 'Data': Class RemotheAuthentication and RemoteAuthenticationParameters. (app)

---
+++ opinion_poll_app/lib/data/usecases/authentication/remote_authentication.dart
@@ -0,0 +1,37 @@
+import '../../../domain/usecases/usecases.dart';
+
+import '../../http/http.dart';
+
+class RemoteAuthenticationParameters {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParameters({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationParameters.fromDomain(
+          AuthenticationParameters parameters) =>
+      RemoteAuthenticationParameters(
+        email: parameters.email,
+        password: parameters.password,
+      );
+
+  Map<String, String> toJson() => {'email': email, 'password': password};
+}
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({
+    required this.httpClient,
+    required this.url,
+  });
+
+  Future<void> authentication(AuthenticationParameters parameters) async {
+    final body = RemoteAuthenticationParameters.fromDomain(parameters).toJson();
+    await httpClient.request(url: url, method: 'POST', body: body);
+  }
+}
