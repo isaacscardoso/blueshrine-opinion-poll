cb6da5e21e52b0090eebf8f22ae6446138677dd7
isaacscardoso <isaacscardosoblue@outlook.com>
2022-12-09 23:13:20 -0300

    (test refactor): Refactoring implementation of tests, after refactoring and implementation of classes and methods. (app)

--- opinion_poll_app/test/data/usecases/authentication/remote_authentication_test.dart
+++ opinion_poll_app/test/data/usecases/authentication/remote_authentication_test.dart
@@ -2,30 +2,11 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:opinion_poll_app/data/usecases/authentication/authentication.dart';
+
 import 'package:opinion_poll_app/domain/usecases/usecases.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({
-    required this.httpClient,
-    required this.url,
-  });
-
-  Future<void> authentication(AuthenticationParameters parameters) async {
-    final body = {'email': parameters.email, 'password': parameters.password};
-    await httpClient.request(url: url, method: 'POST', body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void>? request({
-    required String url,
-    required String method,
-    Map<String, String> body,
-  });
-}
+import 'package:opinion_poll_app/data/http/http.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
