f9d755d205accf49b8ae8f4965a264cc90c0ba50
isaacscardoso <isaacscardosoblue@outlook.com>
2022-12-27 04:25:06 -0300

    Change in 'AccountEntity': Adding 'Factory' to reinsert the class with the access token parameter. (app)

--- opinion_poll_app/lib/domain/entities/account_entity.dart
+++ opinion_poll_app/lib/domain/entities/account_entity.dart
@@ -2,4 +2,7 @@ class AccountEntity {
   final String accessToken;
 
   AccountEntity({required this.accessToken});
+
+  factory AccountEntity.fromJson(Map json) =>
+      AccountEntity(accessToken: json['accessToken']);
 }
