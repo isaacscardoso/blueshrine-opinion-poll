17499a5cb44501458eccdda648e981a44dd8e4b1
isaacscardoso <isaacscardosoblue@outlook.com>
2022-12-29 06:13:30 -0300

    (test refactoring): Summing up the calls to methods. (app)

--- opinion_poll_app/test/data/usecases/authentication/remote_authentication_test.dart
+++ opinion_poll_app/test/data/usecases/authentication/remote_authentication_test.dart
@@ -1,20 +1,21 @@
-import 'package:faker/faker.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:opinion_poll_app/domain/helpers/helpers.dart';
+import 'package:opinion_poll_app/domain/usecases/usecases.dart';
 
 import 'package:opinion_poll_app/data/http/http.dart';
 import 'package:opinion_poll_app/data/usecases/authentication/authentication.dart';
 
-import 'package:opinion_poll_app/domain/helpers/helpers.dart';
-import 'package:opinion_poll_app/domain/usecases/usecases.dart';
+import '../../mocks/mocks.dart';
 
-class HttpClientSpy extends Mock implements HttpClient {}
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
 
 void main() {
   late HttpClientSpy httpClient;
   late String url;
   late RemoteAuthentication systemUnderTest;
   late AuthenticationParameters parameters;
+  late Map<String, String> httpData;
 
   setUp(
     () {
@@ -31,18 +32,12 @@ void main() {
   test(
     'This test is intended to verify that the query to HttpClient is being made with the correct values.',
     () async {
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenAnswer(
-        (_) async => {
-          'accessToken': faker.guid.guid(),
-          'name': faker.person.name(),
-        },
-      );
+      httpData = {
+        'accessToken': faker.guid.guid(),
+        'name': faker.person.name(),
+      };
+
+      httpClient.mockRequest(httpData);
 
       await systemUnderTest.authenticate(parameters: parameters);
 
@@ -50,10 +45,7 @@ void main() {
         () => httpClient.request(
           url: url,
           method: 'POST',
-          body: {
-            'email': parameters.email,
-            'password': parameters.password,
-          },
+          body: {'email': parameters.email, 'password': parameters.password},
         ),
       );
     },
@@ -62,15 +54,10 @@ void main() {
   test(
     'This test is intended to verify that the unexpected error exception is being thrown when HttpClient returns an error 400.',
     () async {
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenThrow(HttpError.badRequest);
+      httpClient.mockRequestError(HttpError.badRequest);
 
       final future = systemUnderTest.authenticate(parameters: parameters);
+
       expect(future, throwsA(DomainError.unexpected));
     },
   );
@@ -78,15 +65,10 @@ void main() {
   test(
     'This test is intended to verify that the unexpected error exception is being thrown when HttpClient returns an error 404.',
     () async {
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenThrow(HttpError.notFound);
+      httpClient.mockRequestError(HttpError.notFound);
 
       final future = systemUnderTest.authenticate(parameters: parameters);
+
       expect(future, throwsA(DomainError.unexpected));
     },
   );
@@ -94,15 +76,10 @@ void main() {
   test(
     'This test is intended to verify that the unexpected error exception is being thrown when HttpClient returns an error 500.',
     () async {
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenThrow(HttpError.internalServerError);
+      httpClient.mockRequestError(HttpError.internalServerError);
 
       final future = systemUnderTest.authenticate(parameters: parameters);
+
       expect(future, throwsA(DomainError.unexpected));
     },
   );
@@ -110,15 +87,10 @@ void main() {
   test(
     'This test is intended to verify that the invalid credentials error exception is being thrown when HttpClient returns an error 401.',
     () async {
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenThrow(HttpError.unauthorized);
+      httpClient.mockRequestError(HttpError.unauthorized);
 
       final future = systemUnderTest.authenticate(parameters: parameters);
+
       expect(future, throwsA(DomainError.invalidCredentialError));
     },
   );
@@ -126,36 +98,25 @@ void main() {
   test(
     'This test is intended to verify that AccountEntity is being returned when the HttpClient returns a status 200.',
     () async {
-      final accessToken = faker.guid.guid();
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenAnswer(
-        (_) async => {
-          'accessToken': accessToken,
-          'name': faker.person.name(),
-        },
-      );
+      httpData = {
+        'accessToken': faker.guid.guid(),
+        'name': faker.person.name(),
+      };
+
+      httpClient.mockRequest(httpData);
 
       final account =
           await systemUnderTest.authenticate(parameters: parameters);
-      expect(account.accessToken, accessToken);
+      expect(account.accessToken, httpData['accessToken']);
     },
   );
 
   test(
     'This test is intended to verify that the unexpected error exception is being thrown when HttpClient returns a status 200 with invalid data.',
     () async {
-      when(
-        () => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body'),
-        ),
-      ).thenAnswer((_) async => {'invalid_key': 'invalid_value'});
+      httpData = {'invalid_key': 'invalid_value'};
+
+      httpClient.mockRequest(httpData);
 
       final future = systemUnderTest.authenticate(parameters: parameters);
       expect(future, throwsA(DomainError.unexpected));
