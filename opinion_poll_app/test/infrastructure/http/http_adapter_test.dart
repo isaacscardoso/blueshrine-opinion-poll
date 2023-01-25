ee766e53a201ae7fe20a02d803b85497ba94633c
isaacscardoso <isaacscardosoblues@outlook.com>
2023-01-25 15:06:32 -0300

    (test implementation): Creating the test to check if any data is being returned when the post method returns status code 200. (app)

--- opinion_poll_app/test/infrastructure/http/http_adapter_test.dart
+++ opinion_poll_app/test/infrastructure/http/http_adapter_test.dart
@@ -5,6 +5,8 @@ import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
+import 'package:opinion_poll_app/data/http/http.dart';
+
 class ClientSpy extends Mock implements Client {
   When _mockRequestCall() => when(() => this
       .post(any(), headers: any(named: 'headers'), body: any(named: 'body')));
@@ -13,12 +15,13 @@ class ClientSpy extends Mock implements Client {
       _mockRequestCall().thenAnswer((_) async => data);
 }
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   late Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  @override
+  Future<Map> request({
     required String url,
     required String method,
     Map? body,
@@ -28,9 +31,11 @@ class HttpAdapter {
       'accept': 'application/json',
     };
 
-    final bodyEncoded = body != null ? jsonEncode(jsonEncode(body)) : null;
+    final bodyEncoded = body != null ? jsonEncode(body) : null;
 
-    await client.post(Uri.parse(url), headers: headers, body: bodyEncoded);
+    final response =
+        await client.post(Uri.parse(url), headers: headers, body: bodyEncoded);
+    return jsonDecode(response.body);
   }
 }
 
@@ -51,7 +56,7 @@ void main() {
       'accept': 'application/json',
     };
     body = {'any_key': 'any_value'};
-    bodyEncoded = jsonEncode(jsonEncode(body));
+    bodyEncoded = jsonEncode(body);
     client.mockRequest(Response('{}', 200));
   });
 
@@ -61,7 +66,7 @@ void main() {
     registerFallbackValue(uri);
   });
 
-  group('post', () {
+  group('POST', () {
     test('Should call the POST method using the correct values.', () async {
       await systemUnderTest.request(url: url, method: 'post', body: body);
 
@@ -72,6 +77,14 @@ void main() {
       await systemUnderTest.request(url: url, method: 'post');
 
       verify(() => client.post(any(), headers: any(named: 'headers')));
+    });
+
+    test('Should return data if the POST method returns status 200', () async {
+      client.mockRequest(Response(bodyEncoded!, 200));
+
+      final response = await systemUnderTest.request(url: url, method: 'post');
+
+      expect(response, body);
     });
   });
 }
