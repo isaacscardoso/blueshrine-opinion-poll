1dff927f3b8cf52bff8bd89f42650ce033500c17
isaacscardoso <isaacscardosoblues@outlook.com>
2023-01-26 16:00:01 -0300

    Adding the 'forbidden' option to the HttpError enumeration, and adding extension to get each status code. (app)

--- opinion_poll_app/lib/data/http/http_error.dart
+++ opinion_poll_app/lib/data/http/http_error.dart
@@ -1,7 +1,27 @@
 enum HttpError {
   badRequest,
-  badGateway,
+  unauthorized,
+  forbidden,
   notFound,
   internalServerError,
-  unauthorized,
+  badGateway,
+}
+
+extension HttpErrorStatusCode on HttpError {
+  int get statusCode {
+    switch (this) {
+      case HttpError.badRequest:
+        return 400;
+      case HttpError.unauthorized:
+        return 401;
+      case HttpError.forbidden:
+        return 403;
+      case HttpError.notFound:
+        return 404;
+      case HttpError.internalServerError:
+        return 500;
+      case HttpError.badGateway:
+        return 502;
+    }
+  }
 }
