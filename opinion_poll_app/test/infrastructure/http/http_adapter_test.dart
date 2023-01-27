9bf39676fae667315170a34de177a73b58aa44eb
isaacscardoso <isaacscardosoblues@outlook.com>
2023-01-26 23:30:10 -0300

    (test implementation): Creating the test to check exceptions in cases of status code: 400, 401, 403, 404, 500. (app)

--- opinion_poll_app/test/infrastructure/http/http_adapter_test.dart
+++ opinion_poll_app/test/infrastructure/http/http_adapter_test.dart
@@ -6,6 +6,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'package:opinion_poll_app/infrastructure/http/http.dart';
+import 'package:opinion_poll_app/data/http/http.dart';
 
 class ClientSpy extends Mock implements Client {
   When _mockRequestCall() => when(() => this
@@ -88,6 +89,46 @@ void main() {
       final response = await systemUnderTest.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequest if method returns status 400', () async {
+      client.mockRequest(body: bodyEncoded!, statusCode: 400);
+
+      final future = systemUnderTest.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return Unauthorized if method returns status 401', () async {
+      client.mockRequest(body: bodyEncoded!, statusCode: 401);
+
+      final future = systemUnderTest.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return Forbidden if method returns status 403', () async {
+      client.mockRequest(body: bodyEncoded!, statusCode: 403);
+
+      final future = systemUnderTest.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
+    test('Should return Not Found if method returns status 404', () async {
+      client.mockRequest(body: bodyEncoded!, statusCode: 404);
+
+      final future = systemUnderTest.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
+    test('Should return Server Error if method returns status 500', () async {
+      client.mockRequest(body: bodyEncoded!, statusCode: 500);
+
+      final future = systemUnderTest.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.internalServerError));
     });
   });
 }
